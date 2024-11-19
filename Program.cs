13fd31deeb571fa4121ffb82a5db5c3476d356e3
phillip deleon <phillipdeleon68@gmail.com>
2024-11-19 10:45:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignments_3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] integer = {{ 2, 3, 4 }, { 1, 4, 6 }}; //2D Array
+
+            for (int i = 0; i < integer.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < integer.GetLength(1); j++)
+                {
+                    Console.Write(integer[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
